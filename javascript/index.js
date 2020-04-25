87f7bb843c2c0c0322120c4182ed1e7676d073e2
NA <nathanmichaelallen@gmail.com>
2020-04-24 20:26:47 -0400

    fix loadPreviews

--- javascript/index.js
+++ javascript/index.js
@@ -25,7 +25,10 @@ function loadPreviews(x) {
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         url: "./sample_interview.html",
     };
-    const data = [data1, data2];
+    let data = [data1, data2];
+    while (data.length > x) {
+        data.pop();
+    }
     writePreviews(data);
 
     // fetch(url)
@@ -117,4 +120,4 @@ function writePreviews(l) {
 }
 
 // Load preview when page is opened
-loadPreviews();
+loadPreviews(2);
